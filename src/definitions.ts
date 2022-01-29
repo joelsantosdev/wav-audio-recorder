8bee09622b0274a39cf60cc231162b942778a184
Joel Santos <joelsantosdev@gmail.com>
2022-01-29 11:01:47 -0600

    Archivos iniciales

---
+++ src/definitions.ts
@@ -0,0 +1,45 @@
+declare module '@capacitor/core' {
+  interface PluginRegistry {
+    WavRecorderPlugin: WavRecorderPluginPlugin;
+  }
+}
+
+export type Base64String = string
+
+export interface RecordingData {
+  value: {
+    recordDataBase64: Base64String
+    msDuration: number
+    mimeType: string
+  };
+}
+
+
+export interface GenericResponse {
+  value: boolean;
+}
+
+export interface CurrentRecordingStatus {
+  status: 'RECORDING' | 'PAUSED' | 'NONE';
+}
+
+
+export interface WavRecorderPluginPlugin {
+  echo(options: { value: string }): Promise<{ value: string }>;
+
+  canDeviceVoiceRecord(): Promise<GenericResponse>;
+
+  requestAudioRecordingPermission(): Promise<GenericResponse>;
+
+  hasAudioRecordingPermission(): Promise<GenericResponse>;
+
+  startRecording(): Promise<GenericResponse>;
+
+  stopRecording(): Promise<RecordingData>;
+
+  pauseRecording(): Promise<GenericResponse>;
+
+  resumeRecording(): Promise<GenericResponse>;
+
+  getCurrentStatus(): Promise<CurrentRecordingStatus>;
+}
---
+++ src/web.ts
@@ -0,0 +1,23 @@
+import { WebPlugin } from '@capacitor/core';
+import { WavRecorderPluginPlugin } from './definitions';
+
+export class WavRecorderPluginWeb extends WebPlugin implements WavRecorderPluginPlugin {
+  constructor() {
+    super({
+      name: 'WavRecorderPlugin',
+      platforms: ['web'],
+    });
+  }
+
+  async echo(options: { value: string }): Promise<{ value: string }> {
+    console.log('ECHO', options);
+    return options;
+  }
+}
+
+const WavRecorderPlugin = new WavRecorderPluginWeb();
+
+export { WavRecorderPlugin };
+
+import { registerWebPlugin } from '@capacitor/core';
+registerWebPlugin(WavRecorderPlugin);
